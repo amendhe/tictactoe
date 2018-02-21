c376e9a40c22cb9f96ad50ca4b886b9058aade7f
Abhijeet Mendhe <abhijeetmendhe7@gmail.com>
2018-02-22 01:14:42 +0530

    error free

--- gameplay/admin.py
+++ gameplay/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Game, Move
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('id','first_player','second_player','status')
+    list_editable = ('status','first_player','second_player')
+
+
+admin.site.register(Move)
+
